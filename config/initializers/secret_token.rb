90d07ad1eb89a69e478c2f6eb331aba209cebf73
Diana Quintal <dquinta@wested.org>
2014-01-18 11:11:04 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChabotAuction::Application.config.secret_token = '2a6e8862491740eabead7f0b064976d7c3f12f0c5a160fcb31d98b1ed83018ede30787af4c94b61211fb8daae0d6583416694f70e7284ae283bb14bfae5b8ef1'
