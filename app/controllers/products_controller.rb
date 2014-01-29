6287f5b587d15b1b248b34cf5cb541dc039f9d20
Diana Quintal <dquinta@wested.org>
2014-01-28 21:16:27 -0800

    decoding the email from the query param

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
-    @bidder = params[:m]
+    @bidder = Base64.decode64(params[:m])
     @categories = Category.all
   end
 
