95eef0538eb00a1b172710a690a03bb952cb0d71
Diana Quintal <dquinta@wested.org>
2014-03-03 22:03:33 -0800

    added bids to products

--- app/controllers/admin/bids_controller.rb
+++ app/controllers/admin/bids_controller.rb
@@ -2,5 +2,6 @@ class Admin::BidsController < ApplicationController
   before_filter :authenticate_user!
   active_scaffold :"bid" do |conf|
     conf.columns = [:product, :email,:amount]
+    conf.list.columns = [:product, :email,:amount, :created_at]
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,10 +4,11 @@ class Admin::ProductsController < ApplicationController
   active_scaffold :"product" do |conf|
 
     conf.columns = [:category,:name,:description,:photo_url, :base_value, :min_bid, :bid_increment,  :donor_name, :contact_id,:open_on_formatted,  :close_on_formatted]
-    conf.list.columns = [:category, :name, :high_bidder_email, :highest_bid_amount,:open_on_formatted,  :close_on_formatted]
+    conf.list.columns = [:category, :name, :high_bidder_email, :highest_bid_amount,:open_on_formatted, :bids, :close_on_formatted]
 
     conf.columns[:close_on_formatted].label = "Close On"
     conf.columns[:open_on_formatted].label = "Open On"
+
   end
 
   def before_update_save(record)
