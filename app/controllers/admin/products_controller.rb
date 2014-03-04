3260320d406579b2af11b582130e51f7ce74afd7
Diana Quintal <dquinta@wested.org>
2014-03-03 22:12:44 -0800

    can sort by high bidder

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,12 +3,13 @@ class Admin::ProductsController < ApplicationController
 
   active_scaffold :"product" do |conf|
 
-    conf.columns = [:category,:name,:description,:photo_url, :base_value, :min_bid, :bid_increment,  :donor_name, :contact_id,:open_on_formatted,  :close_on_formatted]
+    conf.columns = [:category,:name,:description,:photo_url, :base_value, :min_bid, :bid_increment,  :donor_name, :contact_id,:open_on_formatted,  :close_on_formatted,:high_bidder_email]
     conf.list.columns = [:category, :name, :high_bidder_email, :highest_bid_amount,:open_on_formatted, :bids, :close_on_formatted]
 
     conf.columns[:close_on_formatted].label = "Close On"
     conf.columns[:open_on_formatted].label = "Open On"
 
+    conf.columns[:high_bidder_email].sort_by :method => 'high_bidder_email'
   end
 
   def before_update_save(record)
